c51cceb74301724b3e108f24bbb8df8307cb66d2
nithishtech16 <nithishdevtechops16@gmail.com>
2025-08-10 21:58:32 +0530

    Deleting or destroying my already created resources in test environment

--- environments/test/main.tf
+++ environments/test/main.tf
@@ -1,3 +1,4 @@
+/*
 module "vpc" {
   source        = "../../modules/vpc"
   vpc_cidr      = "11.0.0.0/16"
@@ -14,3 +15,4 @@ module "ec2" {
   ec2_count         = 2
   env               = "test"
 }
+*/
