12f45c544a8aaa742a3816318fc7b1e708c9b770
nithishtech16 <nithishdevtechops16@gmail.com>
2025-08-10 21:11:02 +0530

    Added terraform files and jenkins file

---
+++ environments/dev/backend.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.67.0"
+    }
+  }
+  backend "s3" {
+    bucket = "nk1-terraform-bucket"
+    key    = "environments/dev/terraform.tfstate"
+    region = "ap-southeast-1"
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
